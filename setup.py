7ea35ceb8be100e067ee57f7ba19640660062861
sophialee0628 <lsemd96@ewhain.net>
2021-05-20 11:50:33 -0700

    setup.py revised

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read() 
 
 setuptools.setup( 
-name="slee", 
+name="selee", 
 version="0.0.1", 
 author="Seung Eun (Sophia) Lee", 
 author_email="leeseungeun0628@gmail.com", 
